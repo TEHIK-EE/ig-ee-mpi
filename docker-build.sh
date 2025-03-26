83468e5e80daf041886ecdd09e83bc26173ab829
Juri Antonov <juri.antonov@kodality.com>
2025-03-26 23:49:45 +0200

    fix: build including publish url

--- docker-build.sh
+++ docker-build.sh
@@ -6,10 +6,10 @@ if [ -d "$OUTPUT_DIR" ]; then
   rm -Rf OUTPUT_DIR;
 fi
 
-
+version=$(grep '^version:' sushi-config.yaml | awk '{print $2}')
 this="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 if [ ! -f "$this/input-cache/publisher.jar" ]; then
     cd $this && $this/_updatePublisher.sh -y
 fi
 docker run --rm -it -v $this:/workdir -v ~/.fhir:/root/.fhir \
-  docker.kodality.com/fsh-builder /bin/bash -c "cd /workdir && ./_genonce.sh $1"
+  docker.kodality.com/fsh-builder /bin/bash -c "cd /workdir && ./_genonce.sh  $1"
