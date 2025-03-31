c6e31dea2ec69775acf5483b40755b5fb573b9e3
Juri Antonov <juri.antonov@kodality.com>
2025-03-31 12:58:12 +0300

    fix: ig publisher changed scripts

--- docker-build.sh
+++ docker-build.sh
@@ -8,7 +8,7 @@ fi
 
 this="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 if [ ! -f "$this/input-cache/publisher.jar" ]; then
-    cd $this && $this/_updatePublisher.sh -y
+    cd $this && $this/_updatePublisher.sh -f -y
 fi
 docker run --rm -it -v $this:/workdir -v ~/.fhir:/root/.fhir \
-  docker.kodality.com/fsh-builder /bin/bash -c "cd /workdir && ./_genonce.sh  $1"
+  docker.kodality.com/fsh-builder /bin/bash -c "cd /workdir && ./_genonce.sh $1"
