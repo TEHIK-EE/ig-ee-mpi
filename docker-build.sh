47c872a983c1c1825cec85f5363d37697b904350
Juri Antonov <juri.antonov@kodality.com>
2025-05-26 13:20:44 +0300

    fix: use fhir original builder instead of Kodality image

--- docker-build.sh
+++ docker-build.sh
@@ -11,4 +11,4 @@ if [ ! -f "$this/input-cache/publisher.jar" ]; then
     cd $this && $this/_updatePublisher.sh -f -y
 fi
 docker run --rm -it -v $this:/workdir -v ~/.fhir:/root/.fhir \
-  docker.kodality.com/fsh-builder /bin/bash -c "cd /workdir && ./_genonce.sh $1"
+  hl7fhir/ig-publisher-base:latest /bin/bash -c "cd /workdir && ./_genonce.sh $1"
