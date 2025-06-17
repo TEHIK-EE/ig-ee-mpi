64672b55f4db9088c53bb11b951c07748a3d0b04
Juri Antonov <juri.antonov@kodality.com>
2025-06-17 17:42:27 +0300

    feat: UPTIS-3795 implement search

--- local-template/content/assets/js/search-loader.js
+++ local-template/content/assets/js/search-loader.js
@@ -1,15 +1,20 @@
 window.addEventListener('load', ev => {
-  const iframe = document.getElementById('gcse');
-  if (!iframe) {
-    return;
-  }
-  iframe.onload = () => {
-    const doc = iframe.contentDocument || iframe.contentWindow.document;
+  document.getElementById('gcse-box').innerHTML = `<iframe id="gcse" width="100%" height="700" style="border: none;"></iframe>`
 
-    doc.open();
-    // generated with https://programmablesearchengine.google.com/
-    doc.write(`<script async src="https://cse.google.com/cse.js?cx=c5817fbfefdcc42ef"></script><div class="gcse-search"></div>`);
-    doc.close();
-  };
-  iframe.src = 'about:blank';
+  setTimeout(() => {
+    const iframe = document.getElementById('gcse');
+    if (!iframe) {
+      return;
+    }
+    iframe.onload = () => {
+      const doc = iframe.contentDocument || iframe.contentWindow.document;
+
+      doc.open();
+      // generated with https://programmablesearchengine.google.com/
+      doc.write(`<script async src="https://cse.google.com/cse.js?cx=c5817fbfefdcc42ef"></script><div class="gcse-search"></div>`);
+      doc.close();
+    };
+    iframe.src = 'about:blank';
+  });
+
 });
